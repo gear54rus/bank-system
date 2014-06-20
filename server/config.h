251196d07b2cce9d4a39401f5964b16a71cd7c80
Paul <gear54rus@gmail.com>
2014-06-20 13:57:08 +0700

    SERVER: Build configuration reverted to standard state.

--- server/config.h
+++ server/config.h
@@ -5,7 +5,7 @@
 #define CORE_MYSQL_DEFAULT_PORT 3306
 #define LOGGER_DROP_NORMAL_MESSAGE_PREFIX //if defined, messages of level 0 will not have [MSG] prefix
 //#define NETWORK_HANDSHAKE_DUMP // will dump all handshake data received to console
-#define NETWORK_DATA_DUMP // will dump all plaintext data received to console
+//#define NETWORK_DATA_DUMP // will dump all plaintext data received to console
 
 #define QT_NO_CAST_FROM_BYTEARRAY
 
